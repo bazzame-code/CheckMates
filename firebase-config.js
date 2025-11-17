a5faa5a95f5baeda403da304a0a37a2cb780f41f
bazzame-code <bazzame@gmail.com>
2025-11-17 11:26:54 +0000

    Update Firebase configuration with new credentials

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmbXt65flJiLg3gBs0Cn3VAtdiltSiLYw",
+  authDomain: "checkmates-41ac8.firebaseapp.com",
+  databaseURL: "https://checkmates-41ac8-default-rtdb.firebaseio.com",
+  projectId: "checkmates-41ac8",
+  storageBucket: "checkmates-41ac8.firebasestorage.app",
+  messagingSenderId: "27613607258",
+  appId: "1:27613607258:web:06cdc538faf79f5790af63",
+  measurementId: "G-VNC8MSGGCG"
+};
